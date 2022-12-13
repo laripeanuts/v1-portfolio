adf9d5cd926dcc00668b68120a378985482a6154
laripeanuts <larissarabelolf@gmail.com>
2022-12-13 18:59:38 -0300

    feat: adds icon library

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#523db2",
+      },
+    },
   },
   plugins: [],
 };
